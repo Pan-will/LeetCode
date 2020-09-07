5a44fa4e886fd2b572d2c1f8f4042e39790936cc
pw <15097686925@163.com>
2020-09-07 19:30:43 +0800

    深搜、广搜法都写了。

--- 树/剑指 Offer 55 - I. 二叉树的深度.py
+++ 树/剑指 Offer 55 - I. 二叉树的深度.py
@@ -1,8 +1,7 @@
 """
-给定一个二叉树，找出其最大深度。
-二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
-说明: 叶子节点是指没有子节点的节点。
-示例：
+输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+
+例如：
 给定二叉树 [3,9,20,null,null,15,7]，
 
     3
@@ -12,8 +11,6 @@
    15   7
 返回它的最大深度 3 。
 """
-
-
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, x):
@@ -21,24 +18,10 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
-
 class Solution(object):
-    # 递归思路：
-    # （1）如果二叉树为空，二叉树的深度为0
-    # （2）如果二叉树不为空，二叉树的深度 = max(左子树深度， 右子树深度) + 1
+    # 方法一：BFS
+    # 思路：层序模板。
     def maxDepth(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        if not root:
-            return 0
-        depthLeft = self.maxDepth(root.left)
-        depthRight = self.maxDepth(root.right)
-        return depthLeft + 1 if depthLeft > depthRight else depthRight + 1
-
-    # 方法二：BFS
-    def maxDepth2(self, root):
         """
         :type root: TreeNode
         :rtype: int
@@ -59,3 +42,16 @@ class Solution(object):
                     stack.append(node.right)
             ans.append(temp)
         return len(ans)
+
+    # 方法一：DFS
+    # 思路：树的深度 = 根的左子树深度+1 或 右子树深度+1
+    def maxDepth2(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        leftDepth = self.maxDepth2(root.left)
+        rightDepth = self.maxDepth2(root.right)
+        return leftDepth + 1 if leftDepth > rightDepth else rightDepth + 1
