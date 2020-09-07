bed20b08f429354fb1601f296fe74a9ee8d4bbb3
pw <15097686925@163.com>
2020-09-07 08:32:38 +0800

    还是层序模板。

---
+++ 树/515. 在每个树行中找最大值.py
@@ -0,0 +1,47 @@
+"""
+您需要在二叉树的每一行中找到最大的值。
+
+示例：
+
+输入:
+
+          1
+         / \
+        3   2
+       / \   \
+      5   3   9
+
+输出: [1, 3, 9]
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def largestValues(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        ans = []
+        stack = [root]
+        while stack:
+            temp = []
+            sizeStack = len(stack)
+            for i in range(sizeStack):
+                node = stack.pop(0)
+                temp.append(node.val)
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+            ans.append(max(temp))
+        return ans
