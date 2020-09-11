dc67dfdf6f3948acf63c3cd15e42039f3a484e6c
pw <15097686925@163.com>
2020-09-11 15:52:43 +0800

    面试题 04.02. 最小高度树

---
+++ 树/面试题 04.02. 最小高度树.py
@@ -0,0 +1,42 @@
+"""
+给定一个有序整数数组，元素各不相同且按升序排列，编写一个算法，创建一棵高度最小的二叉搜索树。
+
+示例:
+给定有序数组: [-10,-3,0,5,9],
+
+一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+
+          0
+         / \
+       -3   9
+       /   /
+     -10  5
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return None
+        # 二分法找nums中点
+        start, end = 0, len(nums)
+        mid = start + (end - start)//2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        return root
+
+
+
