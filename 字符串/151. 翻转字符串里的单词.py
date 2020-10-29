24662ad7127ba226e8b298dbbf833c0f931ae729
pw <15097686925@163.com>
2020-10-29 12:33:35 +0800

    151

---
+++ 字符串/151. 翻转字符串里的单词.py
@@ -0,0 +1,32 @@
+"""
+给定一个字符串，逐个翻转字符串中的每个单词。
+
+说明：
+无空格字符构成一个 单词 。
+输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ 
+示例 1：
+输入："the sky is blue"
+输出："blue is sky the"
+
+示例 2：
+输入："  hello world!  "
+输出："world! hello"
+解释：输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+"""
+
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        words = s.split()
+        return ' '.join(words[::-1])
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.reverseWords("  hello world!  "))
