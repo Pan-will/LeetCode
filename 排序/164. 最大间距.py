ea0ced9253ae2eac345992edac2576fc561a3a6a
pw <15097686925@163.com>
2020-11-26 09:57:59 +0800

    164

---
+++ 排序/164. 最大间距.py
@@ -0,0 +1,33 @@
+"""
+给定一个无序的数组，找出数组在排序之后，相邻元素之间最大的差值。
+
+如果数组元素个数小于 2，则返回 0。
+
+示例 1:
+
+输入: [3,6,9,1]
+输出: 3
+解释: 排序后的数组是 [1,3,6,9], 其中相邻元素 (3,6) 和 (6,9) 之间都存在最大差值 3。
+示例 2:
+
+输入: [10]
+输出: 0
+解释: 数组元素个数小于 2，因此返回 0。
+"""
+
+
+class Solution(object):
+    def maximumGap(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n < 2:
+            return 0
+        nums.sort()
+        ans = nums[1] - nums[0]
+        for i in range(2, n):
+            if nums[i] - nums[i - 1] > ans:
+                ans = nums[i] - nums[i - 1]
+        return ans
