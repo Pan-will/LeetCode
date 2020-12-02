7cd8ae336290d9fef8fb78d51f3402bd8e1311fb
pw <15097686925@163.com>
2020-12-02 11:30:16 +0800

    周赛

--- 周赛/5614. 找出最具竞争力的子序列.py
+++ 周赛/5614. 找出最具竞争力的子序列.py
@@ -11,30 +11,51 @@
 """
 
 
-# 思路：用回溯法统计长度为k的子序列，用res记录下来; 遍历res返回最有竞争力的那个子序列。   -----超时
 class Solution(object):
+    # 思路：贪心+栈
     def mostCompetitive(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: List[int]
         """
+        # 初始化栈
+        stack = [-1]
+        # 从左往右遍历各位数字，保证原序
+        for i, digit in enumerate(nums):
+            # 若栈顶元素大于当前元素，且剩余未遍历到的数字足够凑齐k个数，则栈顶数字出栈
+            while stack[-1] > digit and len(nums) - i > k - len(stack) + 1:
+                stack.pop(-1)
+            # 栈顶数字小于等于当前数字，或者还没凑够k个数字，则直接进栈
+            stack.append(digit)
+        # 去掉多余的数字，只要k+1个——注意初始化时栈底数字为-1
+        while len(stack) > k + 1:
+            stack.pop(-1)
+        # 返回值抹去前导-1，只要后面的k个数
+        return stack[-k:]
+
+    # 思路：用回溯法统计长度为k的子序列，用res记录下来; 遍历res返回最有竞争力的那个子序列。   -----超时
+    def mostCompetitive2(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
         # 返回值初始化
-        res = nums[:k]
+        res = []
         n = len(nums)
         # 通过回溯找到所有长为k的子序列
         self.dfs(res, nums, n, k, 0, [])
         # 遍历res找到最优竞争力的子序列
-        # ans = res[0]
-        # for i in range(1, len(res)):
-        #     ans = self.betterPower(ans, res[i])
-        # return ans
-        return res
+        ans = res[0]
+        for i in range(1, len(res)):
+            ans = self.betterPower(ans, res[i])
+        return ans
 
     def dfs(self, res, nums, n, k, begin, temp):
         # 定义递归出口：长度为k
-        if len(temp) == k and temp != res and temp == self.betterPower(temp, res):
-            res = temp
+        if len(temp) == k:
+            res.append(temp)
         for i in range(begin, n):
             if len(temp) > k:
                 break
@@ -59,7 +80,8 @@ class Solution(object):
 
 if __name__ == '__main__':
     s = Solution()
+    print(s.mostCompetitive([71, 18, 52, 29, 55, 73, 24, 42, 66, 8, 80, 2], 3))
     print(s.mostCompetitive([3, 5, 2, 6], 2))
-    # print(s.mostCompetitive(
-    #     [84, 10, 71, 23, 66, 61, 62, 64, 34, 41, 80, 25, 91, 43, 4, 75, 65, 13, 37, 41, 46, 90, 55, 8, 85, 61, 95, 71],
-    #     24))
+    print(s.mostCompetitive(
+        [84, 10, 71, 23, 66, 61, 62, 64, 34, 41, 80, 25, 91, 43, 4, 75, 65, 13, 37, 41, 46, 90, 55, 8, 85, 61, 95, 71],
+        24))
