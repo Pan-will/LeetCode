1630e8af96f1e547b18b775cc9997c07a8fc2bab
panwill <panweiwei@zhihu.com>
2021-09-05 01:24:27 +0800

    股神

---
+++ 公司真题/练习/股神.py
@@ -0,0 +1,40 @@
+"""
+经过严密的计算，小赛买了一支股票，他知道从他买股票的那天开始，股票会有以下变化：
+第一天不变，以后涨一天，跌一天，涨两天，跌一天，涨三天，跌一天...依此类推。
+为方便计算，假设每次涨和跌皆为1，股票初始单价也为1，请计算买股票的第n天每股股票值多少钱？
+
+样例输入
+1 2 3 4 5
+
+样例输出
+1 2 1 2 3
+"""
+
+
+def getRes(n):
+    recPri = []
+    while n < 3:
+        print(n)
+        return
+    day = 0  # 一轮开始之前
+    up = 1  # 当前轮连涨带跌的天数，初始化为第一轮
+    while len(recPri) < n:
+        if day < up:
+            recPri.append(1)
+            day += 1
+        elif day == up:  # 每涨完一轮，会跌一天
+            recPri.append(-1)
+            up += 1  # 下一轮涨的天数也递增1天
+            day = 0  # 重新记录天数
+    # print("股票第二天起涨跌趋势：", recPri)
+    price = [1] + [0 for _ in range(n - 1)]
+    for i in range(1, n):
+        price[i] = price[i - 1] + recPri[i - 1]
+    # print("股票价格：", price)
+    print(price[-1])
+
+
+if __name__ == '__main__':
+    while 1:
+        n = int(input())
+        getRes(n)
