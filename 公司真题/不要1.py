baadb80ada89afb056a93c921fca3456e93be53a
pw <15097686925@163.com>
2021-09-06 23:38:47 +0800

    顺丰笔试：不要1

--- 公司真题/不要1.py
+++ 公司真题/不要1.py
@@ -13,8 +13,7 @@ def isHaveOne(n):
     tar = []
     while cur > 0:
         t = cur % 10
-        if t == 1:
-            return True
+        if t == 1: return True
         tar.append(t)
         cur = cur // 10
     print("n的构成：", tar)
@@ -25,16 +24,14 @@ def isHaveOne(n):
     temp.sort(reverse=True)  # 降序排列
     print("预备因数：", temp)
     k = 0  # 遍历指针
-    while n > 0 and k < size:
+    while n > 0 and k < len(temp):
         if helper(n): return True
-        if n % temp[k] == 0:
-            return True
+        if n % temp[k] == 0: return True
         if n < temp[k]:  # 考虑n小于首个数的情况
             k += 1
             continue
         n = n % temp[k]  # 减到不能再减
-        if n in temp:
-            return True
+        if n in temp: return True
         k += 1
     return False
 
