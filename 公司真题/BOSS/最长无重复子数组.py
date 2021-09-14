2c3f8fd9af188ab35cf134e04caaa039413154d1
pw <15097686925@163.com>
2021-09-14 13:09:13 +0800

    boss面试算法题

---
+++ 公司真题/BOSS/最长无重复子数组.py
@@ -0,0 +1,24 @@
+def getAns(string):
+    if not string: return None
+    if len(string) == 1: return string
+    cur = []
+    size = 0
+    res = []  # 记录答案字符
+    # 遍历原串
+    for i, ch in enumerate(string):
+        if ch not in cur:
+            cur.append(ch)
+            size += 1
+        else:
+            # 备份当前为止，最长的无重复子串
+            res = cur if len(res) < len(cur) else res
+            # 若当前字符已存在，拿到下标
+            tar_in = cur.index(ch)
+            cur = cur[tar_in + 1:]
+            cur.append(ch)
+    return "".join(res)
+
+
+if __name__ == '__main__':
+    string = "abcdac"
+    print(getAns(string))
