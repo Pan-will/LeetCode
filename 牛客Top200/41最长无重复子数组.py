690b685048c8e89d26047b6bc48b5f9af7d59cbb
Pan-will <15097686925@163.com>
2021-11-19 14:37:19 +0800

    牛客top:41最长无重复子数组

--- 牛客Top200/41最长无重复子数组.py
+++ 牛客Top200/41最长无重复子数组.py
@@ -1,7 +1,7 @@
 class Solution:
-    # 用队列：当前数字在队列中，则一直出队知道当前元素不在队中，然后将当前元素入队。
+    # 用队列：当前数字在队列中，则一直出队直到当前元素不在队中，然后将当前元素入队。
     # 每入队一个元素都要计算当前队长度，返回值即是队列长度的最大值。
-    def maxLength(self , arr ):
+    def maxLength(self, arr):
         n = len(arr)
         if n < 2: return n
         queue = []
@@ -12,3 +12,8 @@ class Solution:
             queue.append(number)
             ans = max(ans, len(queue))
         return ans
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.maxLength([2, 2, 3, 4, 3]))
